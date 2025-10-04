ed6565d30bd9871323a011b9576d21d755bdd472
CharlesJuniorF <161252667+CharlesJuniorF@users.noreply.github.com>
2025-10-04 19:32:25 -0400

    Finished video tutorial 2

---
+++ Assets/_Scripts/Bumper.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour
+{
+    public float bumpForce = 1f;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("PlayerBall"))
+        {
+            Vector3 myCenter = transform.position;
+            Vector3 contantPoint = collision.GetContact(0).point;
+
+            myCenter.y = contantPoint.y;
+            Vector3 forceVector = contantPoint - myCenter;
+
+            Rigidbody rb = collision.rigidbody;
+            rb.AddForce(forceVector * bumpForce, ForceMode.Impulse);
+        }
+    }
+}
